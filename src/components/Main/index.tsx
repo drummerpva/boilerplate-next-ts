085a1d36f113cda0ce6f283dc752a398486b58ab
Douglas Poma <douglaspoma@yahoo.com>
2021-05-30 23:54:16 -0300

    feat: add default page and styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,14 @@
+import * as S from './styles'
 export const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="https://google.com/google.jpg" alt="Imagem padrão da google" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="https://google.com/google.jpg"
+      alt="Imagem padrão da google"
+    />
+  </S.Wrapper>
 )
